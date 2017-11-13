c0c1ad781f5b6b0f5a0101f9411cc77d9f6ac665
Anselmos <anselmos@users.noreply.github.com>
2017-11-13 11:40:51 +0100

    Updates README.md and install.sh
    Adds installer for vim/tmux/git and adds an cleaner for apt-get packages.

--- README.md
+++ README.md
@@ -1,4 +1,13 @@
 
 # Configurator Warning!
 
-This configurator at this point is only valid for Ubuntu linux. Please be adviced, that you use it on your own risk. Future development will add compatibility for other distributions"
+This configurator at this point is only valid for Ubuntu Linux.
+
+Please be adviced, that you use it on your own risk.
+
+Future development will add compatibility for other distributions
+
+# How TO:
+
+wget -O anselmos_installer.sh https://goo.gl/mmxQNW && chmod +x anselmos_installer.sh
+./anselmos_installer.sh && rm ./anselmos_installer.sh
--- install.sh
+++ install.sh
@@ -1,10 +1,33 @@
 #!/bin/bash
+#author     :anselmos
+#version    :0.2.alpha
+#usage      :./install.sh
 
 function update_release() {
     echo "update_release function"
+    apt-get update
 }
 function install_dependencies(){
     echo "installing dependencies"
+    apt_get_install vim
+    apt_get_install tmux
+    apt_get_install git
+    install_docker
+}
+function apt_get_install(){
+    package_to_install=$1
+    command='apt-get install -y '$package_to_install
+    echo $command
+    $command
+}
+function install_docker(){
+    echo "Installing docker!"
+}
+
+function clear_apt_get(){
+    echo "clearing apt-get archives"
+    apt-get clean
+    apt-get autoclean
 }
 function unmet_release(){
     echo "This configurator at this point is only valid for Ubuntu linux.\nPlease be adviced, that you use it on your own risk.\nFuture development will add compatibility for other distributions"
@@ -16,6 +39,7 @@ if [ $RELEASE_NAME="Ubuntu" ]; then
     echo "Starting configuring new linux"
     update_release
     install_dependencies
+    clear_apt_get
 else
     unmet_release
 fi
